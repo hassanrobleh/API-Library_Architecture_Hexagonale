7184586d51e3786331d0b588bc81430a02a6bb88
hassanrobleh <hassan.alirobleh@gmail.com>
2022-03-02 22:05:50 +0100

    add Book with entity

---
+++ src/domain/Entities/Book.ts
@@ -0,0 +1,37 @@
+
+export interface IBook {
+    id?: number
+    name: string
+    description: string
+    author: string
+    releaseAt: string
+    imageUrl: string
+    fileUrl: string
+    approved: string
+    available: boolean
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+export class Book implements IBook{
+    id?: number
+    available: boolean
+    constructor(
+        readonly name: string,
+        readonly description: string,
+        readonly author: string,
+        readonly releaseAt: string,
+        readonly imageUrl: string,
+        readonly fileUrl: string,
+        readonly approved: string,
+        )
+    {
+        this.available = true
+    }
+
+    changeAvailable(b: boolean) {
+        if(this.available) {
+            this.available = b
+        }
+    }
+} 
