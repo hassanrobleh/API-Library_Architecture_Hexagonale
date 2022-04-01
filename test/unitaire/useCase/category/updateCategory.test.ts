bd38e5b0fd4f87f33f2feff2aa8031db3fa3fcff
hassanrobleh <hassan.alirobleh@gmail.com>
2022-04-01 23:29:54 +0200

    add update category test

--- test/unitaire/useCase/category/updateCategory.test.ts
+++ test/unitaire/useCase/category/updateCategory.test.ts
@@ -1,15 +1,15 @@
+import { MockCategoryProvider } from "../../data/mock/mockCategoryProvider"
+import { UpdateCategory } from '../../../../src/domain/Application/Category/UseCase/UpdateCategory/UpdateCategory';
+import { category } from "../../data/categoryData";
 
-import { UpdateBook } from '../../../../src/domain/Application/Book/UseCase/UpdateBook/UpdateBook';
-import { MockBookProvider } from '../../data/mock/mockBookProvider';
-import { book } from '../../data/bookData';
 
 
 describe('add book', () => {
 
-    const mockbookProvider = new MockBookProvider()
-    const updateBook = new UpdateBook(mockbookProvider)
+    const mockCategoryProvider = new MockCategoryProvider()
+    const updateCategory = new UpdateCategory(mockCategoryProvider)
     it('add book', async () => {
-        const { message } = await updateBook.execute(1, book)
-        expect(message).toBe('Le book a bien été modifié')
+        const { message } = await updateCategory.execute(1, category)
+        expect(message).toBe('Le category a bien été modifié')
     })
 })
