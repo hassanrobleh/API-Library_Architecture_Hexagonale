39f5b3e0dcdf5b5437970fcef1aec9be264535ec
hassanrobleh <hassan.alirobleh@gmail.com>
2022-04-07 23:35:37 +0200

    add user route

---
+++ src/infrastructure/typeorm/routes/User.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+
+
+const userRouter = Router()
+
+userRouter.get('/')
+
+userRouter.get('/:id')
+
+userRouter.post('/')
+
+userRouter.put('/:id')
+
+userRouter.delete('/:id')
+
+export default userRouter
