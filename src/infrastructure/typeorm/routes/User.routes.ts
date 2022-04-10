e9d4608a41587b1cb3112c36265b055c4d732857
hassanrobleh <hassan.alirobleh@gmail.com>
2022-04-10 04:24:46 +0200

    update user router

--- src/infrastructure/typeorm/routes/User.routes.ts
+++ src/infrastructure/typeorm/routes/User.routes.ts
@@ -1,16 +1,13 @@
 import { Router } from 'express'
+import { getUser, getUsers, createUser, updateUser, deleteUser } from '../controllers/UserController';
 
 
 const userRouter = Router()
 
-userRouter.get('/')
-
-userRouter.get('/:id')
-
-userRouter.post('/')
-
-userRouter.put('/:id')
-
-userRouter.delete('/:id')
+userRouter.get('/', getUsers)
+userRouter.get('/:id', getUser)
+userRouter.post('/', createUser)
+userRouter.put('/:id', updateUser)
+userRouter.delete('/:id', deleteUser)
 
 export default userRouter
--- src/infrastructure/typeorm/routes/index.ts
+++ src/infrastructure/typeorm/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import bookRouter from './Book.routes'
 import categoryRouter from './Category.routes';
+import userRouter from './User.routes';
 
 const router = Router()
 
 router.use('/book', bookRouter)
 router.use('/category', categoryRouter)
+router.use('.user', userRouter)
 
 export default router
 
