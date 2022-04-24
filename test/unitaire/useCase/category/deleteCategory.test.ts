4c80e5b217bee4c27c1ac855790d03092553d91d
hassanrobleh <hassan.alirobleh@gmail.com>
2022-04-24 17:45:40 +0200

    fix bug cate

--- test/unitaire/useCase/category/deleteCategory.test.ts
+++ test/unitaire/useCase/category/deleteCategory.test.ts
@@ -6,7 +6,9 @@ describe('delete category', () => {
     const mockCategoryProvider = new MockCategoryProvider()
     const deleteCategory = new DeleteCategory(mockCategoryProvider)
     it('deletecategory', async () => {
+
         const {message} = await deleteCategory.execute(1)
+        console.log('ok')
         expect(message).toBe('Le category a bien été supprimé')
     })
 })
