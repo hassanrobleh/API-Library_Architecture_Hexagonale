2a0d098e514eeb4b16fdbed641cebcdc0b636fe6
hassanrobleh <hassan.alirobleh@gmail.com>
2022-04-25 02:52:05 +0200

    Fix bug file addbook

--- src/domain/Application/Book/UseCase/GetBooks/GetBooks.ts
+++ src/domain/Application/Book/UseCase/GetBooks/GetBooks.ts
@@ -1,13 +1,16 @@
-import { IBookRepository } from '../../../../Repository/BookRepository';
-import { GetBooksResponse } from './GetBooksResponse';
+import { IBookRepository } from '../../../../Repository/BookRepository'
+import { GetBooksResponse } from './GetBooksResponse'
 
 export class GetBooks {
-
     constructor(private repository: IBookRepository) {}
 
     async execute() {
-        const response = new GetBooksResponse()
-        response.books = await this.repository.getBooks()
-        return response
+        try {
+            const response = new GetBooksResponse()
+            response.books = await this.repository.getBooks()
+            return response
+        } catch (error) {
+            throw new Error(error)
+        }
     }
 }
