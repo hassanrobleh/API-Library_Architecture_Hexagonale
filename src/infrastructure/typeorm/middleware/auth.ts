10c742bd90b83986d44f6ca88f1ad6c64b396fb8
Hassan <30626086+hassanrobleh@users.noreply.github.com>
2022-05-09 01:25:24 +0200

    Update auth.ts

--- src/infrastructure/typeorm/middleware/auth.ts
+++ src/infrastructure/typeorm/middleware/auth.ts
@@ -5,7 +5,7 @@ export const auth = (req, res, next) => {
 
     if (!authorizationHeader) {
         const message = `Vous n'avez pas fourni de jeton d'authentification. Ajoutez-en un dans l'en-tête de la requête.`
-        return res.status(401).json({ message })
+        
     }
 
     const token = authorizationHeader.split(' ')[1]
